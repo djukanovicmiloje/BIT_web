c0b6160650161302f69cc96698eb457cd4c2fe7e
BIT Student <classroom@bgit.rs>
2019-11-20 14:53:26 +0100

    BitShow sa casa

--- BitShow/js/ctrl.js
+++ BitShow/js/ctrl.js
@@ -1,13 +1,55 @@
 const controllerModule = (function (data, ui) {
     const NUM = 50;
 
-    data.fetchShows(NUM, ui.createShowElements);
+    function generateShowList() {
 
-    $('#search').on('keyup', function(){
+        $shows = $('<div>');
+        $shows.attr('id', 'shows');
+        $('#content').append($shows);
+
+        data.fetchShows(NUM, function (showList) {
+            for (let i = 0; i < showList.length; i++) {
+                let $show = ui.createShowElement(showList[i]);
+
+                $show.click(function () {
+                    ui.generateInfoScreen(showList[i].id, data.fetchAShow)
+                })
+            }
+        });
+
+    }
+
+
+    generateShowList();
+
+
+    $dropdown = $('#dropdown');
+    $search = $('#search');
+
+    const dropDownLength = 8;
+
+    $search.on('keyup', function () {
+
+        $dropdown.html('');
+
         let searchValue = ui.collectSearchValue();
-        //console.log(searchValue);
-        
-    });
 
+        data.fetchShows(dropDownLength, function (showList) {
+
+            for (let i = 0; i < showList.length; i++) {
+                $searchItem = ui.generateSearchDropdownItem(showList[i]);
+                $searchItem.click(function () {
+                    ui.generateInfoScreen(showList[i].id, data.fetchAShow);
+                    $search.next('div').html('');
+                })
+
+                $dropdown.append($searchItem);
+
+
+            }
+
+        }, searchValue)
+
+    });
 
 })(dataModule, uiModule);
--- BitShow/js/ui.js
+++ BitShow/js/ui.js
@@ -1,57 +1,111 @@
 const uiModule = (function () {
 
+    $content = $('#content');
 
-    function createShowElements(showList) {
+    function createShowElement(show) {
         $parent = $('#shows');
 
-        for (let i = 0; i < showList.length; i++) {
-
-            let $show = $('<div>');
-            $show.addClass('show');
-
-            let $image = $('<img>');
-            $image.attr('src', showList[i].image);
-            $show.append($image);
-
-            let $title = $('<h2>');
-            $title.text(showList[i].name);
-            $show.append($title);
-
-            $show.click(function(){
-                generateInfoScreen(showList[i]);
-            })
-
-            $parent.append($show);
-        }
-    }
-    function generateInfoScreen(show){
-        let $infoScreen = $('<div>');
-        
-        let $title = $('<h1>');
-        $title.text(show.name);
-
-        $infoScreen.append($title);
+        let $show = $('<div>');
+        $show.addClass('show');
 
         let $image = $('<img>');
         $image.attr('src', show.image);
+        $show.append($image);
 
-        $infoScreen.append($image);
+        let $title = $('<h2>');
+        $title.text(show.name);
+        $show.append($title);
+
+        $parent.append($show);
+
+        return $show;
+
+    }
+    function generateInfoScreen(id, getData) {
+
+        getData(id, function (show) {
+
+            let $infoScreen = $('<div>');
+            $infoScreen.attr('class', 'row');
+            $infoScreen.attr('id', 'infoScreen');
+
+            let $title = $('<h1>');
+            $title.attr('class', 'col-12');
+            $title.text(show.name);
+
+            $infoScreen.append($title);
+
+            let $image = $('<img>');
+            $image.attr('class', 'col-6');
+            $image.attr('src', show.image);
+
+            $infoScreen.append($image);
 
 
-////////////////////////////////////////////////////
+            let $container = $('<div>');
+            $container.attr('class', 'col-6 row');
+
+            let $seasons = $('<div>');
+            $seasons.attr('class', 'col-12');
+            $seasons.append($('<span>').text(`Seasons (${show.seasons.length}):`));
+
+            for (let i = 0; i < show.seasons.length; i++) {
+                let $season = $('<li>');
+                $season.text(show.seasons[i]);
+
+                $seasons.append($season);
+            }
+
+            $container.append($seasons);
+
+            let $cast = $('<div>');
+            $cast.attr('class', 'col-12');
+            $cast.append($('<span>').text('Cast:'));
 
 
-        $('#shows').replaceWith($infoScreen);
+            for (let i = 0; i < show.cast.length; i++) {
+                $actor = $('<li>');
+                $actor.text(show.cast[i]);
+                $cast.append($actor);
+            }
+
+
+            $container.append($cast);
+
+            $infoScreen.append($container);
+
+            $summary = $(show.summary);
+            $summary.attr('class', 'col-12');
+
+            $infoScreen.append($summary);
+
+            $content.html('');
+            $content.append($infoScreen);
+
+        });
+
     }
 
     $searchInput = $('#search');
+    $searchBar = $('#searchBar');
 
-    function collectSearchValue(){
+    function generateSearchDropdownItem(show) {
+
+        let $show = $('<li>');
+        $show.attr('class', 'dropDownItem');
+        $show.text(show.name);
+
+        return $show;
+    }
+
+    function collectSearchValue() {
         return $searchInput.val();
     }
 
     return {
-        createShowElements,
-        collectSearchValue
+        createShowElement,
+        collectSearchValue,
+        generateInfoScreen,
+        generateSearchDropdownItem
     }
 })()
